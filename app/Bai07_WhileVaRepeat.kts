337d7ea89d7db50c51c17002eac2e206c0b588b0
Nguyen Manh Tien <tiennguyenmanh2006@gmail.com>
2026-09-25 09:58:33 +0700

    Buoi02 Bai07_WhileVaRepeat.kts

---
+++ app/Bai07_WhileVaRepeat.kts
@@ -0,0 +1,15 @@
+//25810043
+//Nguyen Manh Tien
+fun main() {
+    var dem = 10
+    while (dem >= 1) {
+        println("Dem nguoc: $dem")
+        dem--
+    }
+
+    repeat(5) {
+        println("Dong co dinh")
+    }
+}
+
+main()
