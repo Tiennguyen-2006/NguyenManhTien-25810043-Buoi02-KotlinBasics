bb759528013d17220a1398ce2262345db7044ac1
Nguyen Manh Tien <tiennguyenmanh2006@gmail.com>
2026-09-25 09:53:22 +0700

    Buoi02 Bai03_TaiKhoanNganHang.kts

---
+++ app/Bai03_TaiKhoanNganHang.kts
@@ -0,0 +1,13 @@
+//25810043
+//Nguyen Manh Tien
+val soDuBanDau: Double = 5000000.0
+var soDuHienTai: Double = soDuBanDau
+
+fun main() {
+    soDuHienTai += 2000000.0
+    println("So du: $soDuHienTai VND")
+
+    soDuHienTai -= 1500000.0
+    println("So du: $soDuHienTai VND")
+}
+main()
