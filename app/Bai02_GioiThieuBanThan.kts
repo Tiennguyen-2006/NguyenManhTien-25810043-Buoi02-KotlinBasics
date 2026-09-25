d49fe61590ebf1a5bd8cdd323b359fda9bcc871c
Nguyen Manh Tien <tiennguyenmanh2006@gmail.com>
2026-09-25 09:46:20 +0700

    Buoi2 Bai02_GioiThieuBanThan.kts

---
+++ app/Bai02_GioiThieuBanThan.kts
@@ -0,0 +1,10 @@
+//25810043
+//Nguyen Manh Tien
+fun main() {
+    val hoTen: String = "Nguyen Manh Tien"
+    val namSinh: Int = 2006
+    val namHienTai: Int = 2026
+    val tuoi: Int = namHienTai - namSinh
+    println("Toi ten la $hoTen, nam nay toi $tuoi tuoi.")
+}
+main()
